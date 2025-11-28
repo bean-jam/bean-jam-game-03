41f9f18ec921ee53d5f2be65221c0a603cc37f9b
Elliott Feltham <elliottfeltham98@gmail.com>
2025-11-28 10:51:55 +0000

    Add a proper queuing system

--- NOTES.md
+++ NOTES.md
@@ -1,1 +1,14 @@
 # Notes
+
+Create the GameManager and SignalBus singletons
+
+Create a GhostSpawner to easily spawn multiple ghosts in the queue
+
+Create text boxes that pop up on the "booth_touched" signal
+
+Create a DialogManager to handle the text 
+
+Add a set of dialog where characters tell you about their lives and you decide whether
+they go to heaven or hell based on the ruleset which is currently active.
+For example, if someone says they only ever ate fish on a Thursday but the rules say
+anything but Friday is a sin... straight to hell buddy.
--- scripts/Ghost.gd
+++ scripts/Ghost.gd
@@ -2,16 +2,49 @@ extends CharacterBody2D
 class_name Ghost
 ## The base script for all ghost characters
 
-var tween = create_tween()
+## Adjust the speed the ghosts move in the queue
+@export var movement_speed: float = 10.0
+
+## Used to set speed back to movement speed after every stop.
+var speed = movement_speed
+
+## The forward raycast to detect collisions
+@onready var ray: RayCast2D = $InteractionRaycast
+
+## This signal checks whether the booth has been touched.
+## Will be moved to the global signal bus eventually.
+signal booth_touched(ghost: Node)
 
 func _ready():
-	# Start the initial bobbing loop
-	bob_up_down()
+	randomize()
+	add_to_group("ghosts")
+	booth_touched.connect(_on_booth_touched)
 
-func bob_up_down():
-	# Set the tween to loop
-	tween.set_loops()
-	# Move up
-	tween.tween_property(self, "position:y", position.y - 1, 1)
-	# Chain the down movement
-	tween.tween_property(self, "position:y", position.y + 1, 1)
+## Temporary disappearing to test queueing
+func _on_booth_touched(ghost):
+	await get_tree().create_timer(5.0).timeout
+	queue_free()
+
+## This is the queue physics and movement.
+## The ghosts stop when they reach the desk or the next ghost in line
+func _physics_process(delta: float) -> void:
+	if ray.is_colliding():
+		var collider = ray.get_collider()
+		if not collider:
+			return
+		
+		# If it's the booth, emit the signal and stop
+		if collider.is_in_group("booth"):
+			booth_touched.emit(self)
+			speed = 0.0
+			return
+
+		# Otherwise, stop if it's another ghost
+		if collider.is_in_group("ghosts"):
+			speed = 0.0
+			return
+
+	# no blocking object ahead -> keep moving
+	speed = movement_speed
+	velocity = Vector2(0, -1) * speed
+	move_and_slide()
